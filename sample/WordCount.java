d4d775c3d6795bf7a7f2bc0ca204156eb09e4983
coatless <coatless@users.noreply.github.com>
2015-01-22 11:53:12 -0600

    Update WordCount.java

--- sample/WordCount.java
+++ sample/WordCount.java
@@ -21,7 +21,7 @@ import org.apache.hadoop.mapreduce.Counter;
 import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.hadoop.util.StringUtils;
 
-public class WordCount2 {
+public class WordCount {
 
   public static class TokenizerMapper
        extends Mapper<Object, Text, Text, IntWritable>{
