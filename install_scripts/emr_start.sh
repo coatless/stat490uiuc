ddb314924ede14855d5750d6c987a2643ec1fab0
coatless <balamut2@illinois.edu>
2015-02-09 10:09:47 -0600

    More readable...

--- install_scripts/emr_start.sh
+++ install_scripts/emr_start.sh
@@ -1,5 +1,23 @@
-bucket="stat490uiuc"
-region="us-east-1"
-keypair="<hidden>"
+bucket="<YOUR_BUCKET>"
+region="<YOUR_REGION>"
+keypair="<YOUR_KEYPAIR>"
+master_instance="m1.large"
+slave_instance="m1.large"
+num_slaves=2
 
-aws emr create-cluster --name emR-example --ami-version=3.3.0 --applications Name=Hue Name=Hive Name=Pig --ec2-attributes KeyName=myKey --region $region --use-default-roles --ec2-attributes KeyName=$keypair --no-auto-terminate --instance-groups InstanceGroupType=MASTER,InstanceCount=1,InstanceType=m1.large InstanceGroupType=CORE,InstanceCount=2,InstanceType=m1.large --bootstrap-actions Name=hdp_setup,Path="s3://$bucket/hdp_setup.sh",Args=[--emrinstall,--rstudio,--createuser,--sudouser,--sshuser,--hpaths,--rhadoop,--rhpc] --steps Name=HDFS_tmp_permission,Jar="s3://elasticmapreduce/libs/script-runner/script-runner.jar",Args="s3://$bucket/emr_permissions.sh"
+aws emr create-cluster --name emr_cluster \
+--ami-version 3.2.1 \
+--region $region \
+--ec2-attributes KeyName=$keypair \
+--no-auto-terminate \
+--instance-groups \
+InstanceGroupType=MASTER,InstanceCount=1,InstanceType=$master_instance \
+InstanceGroupType=CORE,InstanceCount=$num_slaves,InstanceType=$slave_instance \
+--bootstrap-actions \
+Name=emR_bootstrap,\
+Path="s3://$bucket/hdp_setup.sh",\
+Args=[--emrinstall,--rstudio,--hpaths,--rhadoop,--createuser,--sudouser,--sshuser] \
+--steps \
+Name=HDFS_tmp_permission,\
+Jar="s3://elasticmapreduce/libs/script-runner/script-runner.jar",\
+Args="s3://$bucket/emr_permissions.sh"
